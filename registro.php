6003b7d7479ebf978cc9a5422ed023f502644cf1
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-10-25 17:59:26 -0300

    estructura html

--- registro.php
+++ registro.php
@@ -9,7 +9,26 @@
     <div class="principal">
         <div class="formulario">
             <form action="" method="post">
-                label
+                <div class="titulo">
+                    <h1>Regístrate</h1>
+                </div>
+                <label for="">Ingresa tu nombre</label>
+                <br>
+                <input type="text" name="nombre" id="">
+                <br>
+                <label for="">Ingresa tu correo</label>
+                <br>
+                <input type="email" name="correo" id="">
+                <br>
+                <label for="">Ingresa un usuario</label>
+                <br>
+                <input type="text" name="usuario" id="">
+                <br>
+                <label for="">Ingresa una contraseña</label>
+                <br>
+                <input type="password" name="contrasena" id="">
+                <br>
+                <button>Envía tus datos!</button>
             </form>
         </div>
     </div>
